f2fddc22e99dcf332f3c4f87ae897bb4ccb834df
Bram Vereertbrugghen <bram@adimian.com>
2019-10-29 22:40:30 +0100

    Initial commit, continuation of previous project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cson To Markdown
---
+++ tests/test_cson_to_markdown.py
@@ -0,0 +1,17 @@
+from cson_to_markdown import __version__
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+class TestMarkdownExtractor:
+    pass
+
+
+class TestMarkdownWriter:
+    pass
+
+
+class TestMetadataWriter:
+    pass
