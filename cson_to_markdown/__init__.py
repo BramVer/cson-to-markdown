66aa1fb805ab1e8ba4c43285de9121e54933fee9
Bram Vereertbrugghen <bram@adimian.com>
2019-11-02 22:29:55 +0100

    Added text statements for readability

--- cson_to_markdown/__init__.py
+++ cson_to_markdown/__init__.py
@@ -6,8 +6,14 @@ __version__ = "0.1.0"
 
 
 def _convert(og_path, new_path=None):
+    start_msg = f'Start .cson to .md conversion on "{og_path}".'
+    print(start_msg)
+
     converter = FileConverter(og_path, new_path)
     converter.convert()
+
+    end_msg = f'Successfully converted files to "{new_path}".'
+    print(end_msg)
 
 
 def main():
